5e13674dbee9d7e239d2fca58cf62dca0e4cfe16
Faris Rasyid <frisrsyd@gmail.com>
2022-04-19 17:32:07 +0700

    adding edit and delete post
    tambah fitur edit dan delete pada halaman my profile

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [PostController::class, 'index']);
 Route::get('/home', [PostController::class, 'index']);
 Route::get('/sign-in', [PostController::class, 'signIn']);
 Route::get('/sign-up', [PostController::class, 'signUp']);
 Route::get('/forgotPass', [PostController::class, 'forgot']);
 Route::get('/createpost', [PostController::class, 'createPost']);
 Route::get('/profile', [PostController::class, 'profile']);
+Route::get('/edit-post/{id}', [PostController::class, 'editPost']);
+Route::get('/delete-post/{id}', [PostController::class, 'deletePost']);
+
+Route::put('/update-post/{id}', [PostController::class, 'updatePost']);
 
 Route::post('createpost', [PostController::class, 'store']);
